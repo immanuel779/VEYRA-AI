7421a2bfb7410035f70d9a8dbbe2d94f3943b3fd
Opeyemi <opeyemioluwadamilare415@gmail.com>
2026-09-14 23:28:12 +0100

    fix: remove unused useEffect import from LandingPage

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import {
   ArrowRight, MessageSquare, Code2, Image as ImageIcon, FileText,
   Globe, Mic, Sparkles, Check, Menu, X,
